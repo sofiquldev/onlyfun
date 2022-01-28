859a99413c82ddee0578e6ddab50349a94010876
Sofiqul Islam <sofiquldev@gmail.com>
2022-01-29 04:58:40 +0600

    responsive design

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,65 +1,28 @@
-(function ($) {
-    'use strict';
-
-    // facts number counter up
-    $('.fact-counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-    // Partner logo slider
-    $('.partners-logo').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        responsive: {
-            0: {
-                items: 2
-            },
-            480: {
-                items: 3
-            },
-            992: {
-                items: 4
-            }
-        }
-    })
+// suggestion slider
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".sliderNext",
+        prevEl: ".sliderPrev",
+    },
+});
 
 
-    // Testimonial slider
-    $('.test-caro').owlCarousel({
-        autoplay: true,
-        loop: true,
-        responsiveClass: true,
-        items: 1
-    })
+// mobile menu
+//======================
+$('#menuToggler').on('click', function (e) {
+    e.preventDefault();
+    $('#left-sidebar').slideToggle();
+});
 
-    //======================
-    // Mobile menu 
-    //======================
-    $('#menu-toggler').on('click', function (e) {
-        e.preventDefault();
-        $('.navbar-nav').slideToggle();
-    })
-    $('.has-menu-child').append('<i class="menu-dropdown ti-angle-down"></i>');
-
-    if ($(window).width() <= 991) {
-        $('.menu-dropdown').on('click', function () {
-            $(this).prev().slideToggle();
-            $(this).toggleClass('ti-angle-down ti-angle-up')
-        })
-    }
-
-
-    // skity menubar
-    // let header = document.querySelector('.header')
-
-    window.onscroll = function() {
-    if(window.pageYOffset > 200) {
-        $('.header').addClass('fixed-header')
-    } else {
-        $('.header').removeClass('fixed-header')
-    }
-    }
-
-})(jQuery);
+// suggestion Toggler
+$('#suggestionToggler').on('click', function (e) {
+    e.preventDefault();
+    $('#right-sidebar').slideToggle();
+});
